6e4f12e714326524675a6fe46597a0a0580939aa
Ian Wijma <ian@wij.ma>
2020-08-31 20:18:05 +1000

    Fixed PWA support.

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const withPWA = require('next-pwa')
 module.exports = withPWA({
     pwa: {
       disable: process.env.NODE_ENV === 'development',
-      register: true,
-      scope: '/',
-      sw: 'service-worker.js',
     },
     webpack: (config, { isServer }) => {
         // Fixes npm packages that depend on `fs` module
