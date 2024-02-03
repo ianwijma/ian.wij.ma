d18b605c93c1644ecd268e6418336fc970087e90
Ian Wijma <ian@wij.ma>
2024-02-04 01:26:18 +1100

    Speed up the page loading

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,12 +5,12 @@ export default class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin='true' />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
                     <link
                         href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
                         rel="stylesheet" />
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-143518517-1"></script>
+                    <script src="https://www.googletagmanager.com/gtag/js?id=UA-143518517-1" defer strategy="lazyOnload"></script>
                     <script
                         dangerouslySetInnerHTML={{
                             __html: `window.dataLayer = window.dataLayer || [];
