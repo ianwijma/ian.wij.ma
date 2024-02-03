c1b1ab4d868b3c8b56ac7c6cf4779887bf39ac73
Ian Wijma <ian@wij.ma>
2024-02-04 01:37:06 +1100

    Added Analytics, removed google tags

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,5 +1,6 @@
 import Document, {Html, Head, Main, NextScript} from 'next/document'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 
 export default class MyDocument extends Document {
     render() {
@@ -11,21 +12,12 @@ export default class MyDocument extends Document {
                     <link
                         href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
                         rel="stylesheet" />
-                    <script src="https://www.googletagmanager.com/gtag/js?id=UA-143518517-1" defer strategy="lazyOnload"></script>
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-143518517-1');`
-                        }}
-                    />
                 </Head>
                 <body>
                     <Main/>
                     <NextScript/>
                     <SpeedInsights />
+                    <Analytics />
                 </body>
             </Html>
         )
